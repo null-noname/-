8d50bed7994554f06cc5f97c4b1eb9db2d55b103
null-noname <1429noname@gmail.com>
2025-12-10 16:45:26 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('kakeibo-cache-v1').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './sw.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
