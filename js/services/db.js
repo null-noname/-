0286cff6e1113a76540f6ec52f1345d50463e486
null-noname <1429noname@gmail.com>
2025-12-26 04:48:33 +0900

    Add files via upload

--- js/services/db.js
+++ js/services/db.js
@@ -6,10 +6,13 @@ export const initDB = (path, onData) => {
     dbRef = db.ref(path);
     dbRef.on('value', (s) => {
         const val = s.val();
-        if (val) {
-            state = val;
-            onData(state);
+        if (val) state = val;
+        else {
+            const local = localStorage.getItem('kakeibo_backup');
+            if (local) state = JSON.parse(local);
         }
+        if (!state.years) state.years = { "2025": { months: {} } };
+        onData(state);
     });
 };
 
